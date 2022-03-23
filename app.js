9c90687ec54d07e00026bfd7f8a81b73205762aa
RakulAgn <rakul0agn@gmail.com>
2022-03-23 20:44:21 +0530

    Delete

--- app.js
+++ app.js
@@ -1,90 +1,167 @@
 const express = require("express");
 const app = express();
 const { MongoClient } = require("mongodb");
-const URL = "mongodb+srv://MonkAno:MonkAno@cluster0.cfvup.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const URL =
+  "mongodb+srv://MonkAno:MonkAno@cluster0.cfvup.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const client = new MongoClient(URL);
 
 const PORT = 8080;
 client.connect();
-
-
 app.use(express.json());
 
-
 app.get("/newblog", async (req, res) => {
-    try {
+  // Inserted Data
+  const date = Math.floor(new Date().getTime() / 1000.0);
+  const Post_id = Math.floor(Math.random() * 10000);
+  let BlogTitle = req.query.BlogTitle;
+  let BlogContent = req.query.BlogContent;
+  let myBlogData = { BlogTitle, BlogContent, date, Post_id };
 
-        // Inserted Data
-        const date = Math.floor(new Date().getTime() / 1000.0);
-        const Post_id = Math.floor(Math.random() * 10000);
-        let BlogTitle = req.query.BlogTitle;
-        let BlogContent = req.query.BlogContent;
-        let myBlogData = { BlogTitle, BlogContent, date, Post_id };
+  const allBlog = await client
+    .db("LinkedList")
+    .collection("Sample data")
+    .find()
+    .toArray();
 
+  const lastBlog = allBlog[allBlog.length - 1];
 
-        const allBlog = await client.db("LinkedList").collection("Sample data").find().toArray();
+  if (allBlog.length === 0) {
+    myBlogData = { ...myBlogData, prev: null, next: null };
+  } else {
+    const updatedBlog = {
+      ...lastBlog,
+      next: Post_id,
+    };
+    client
+      .db("LinkedList")
+      .collection("Sample data")
+      .findOneAndUpdate(
+        { Post_id: lastBlog.Post_id },
+        { $set: updatedBlog },
+        { ReturnDocument: "after" }
+      );
+    myBlogData = { ...myBlogData, prev: lastBlog.Post_id, next: null };
+  }
 
-        const lastBlog = allBlog[allBlog.length - 1];
+  const insertNewBlog = await client
+    .db("LinkedList")
+    .collection("Sample data")
+    .insertOne(myBlogData);
+  res
+    .status(200)
+    .send(
+      "<script>window.location.href = 'http://127.0.0.1:5500//index.html'; </script>"
+    );
+});
 
-        if (allBlog.length === 0) {
-            myBlogData = { ...myBlogData, prev: null, next: null };
-        } else {
-            const updatedBlog = {
-                ...lastBlog,
-                next: Post_id,
-            };
-            client.db("LinkedList").collection("Sample data").findOneAndUpdate(
-                { Post_id: lastBlog.Post_id },
-                { $set: updatedBlog },
-                { ReturnDocument: "after" }
-            );
-            myBlogData = { ...myBlogData, prev: lastBlog.Post_id, next: null };
-        }
+app.get("/allblog", async (req, res) => {
+  const blog = await client
+    .db("LinkedList")
+    .collection("Sample data")
+    .find()
+    .toArray();
+  res.status(200).send(blog);
+});
 
-        const insertNewBlog = await client.db("LinkedList").collection("Sample data").insertOne(myBlogData);
-        res
-            .status(200)
-            .send(
-                "<script>window.location.href = 'http://127.0.0.1:5500//index.html'; </script>"
-            );
-    } catch (e) {
-        console.log(e);
-        res.status(422).send("Error in Posting");
-    }
-},
-);
+app.get("/getSingleBlog/:id", async (req, res) => {
+  const id = req.params.id;
+  const singleBlog = await client
+    .db("LinkedList")
+    .collection("Sample data")
+    .findOne({ Post_id: Number(id) });
+  res.send(singleBlog);
+});
 
-app.get("/allblog", (req, res) => {
+app.delete("/deleteBlog/:id", async (req, res) => {
+  const id = req.params.id;
+  const allBlog = await client
+    .db("LinkedList")
+    .collection("Sample data")
+    .find()
+    .toArray();
+  console.log(allBlog);
+  const currentBlog = await client
+    .db("LinkedList")
+    .collection("Sample data")
+    .findOne({ Post_id: Number(id) });
+  console.log(currentBlog);
 
-    async function findAllBlog(client) {
+  if (currentBlog.prev === null) {
+    allBlog.map((blog) => {
+      if (blog.prev === currentBlog.Post_id) {
+        const updatedBlog = {
+          ...blog,
+          prev: null,
+        };
 
-        const allBlog = client.db("LinkedList").collection("Sample data").find();
-        const result = await allBlog.toArray();
-        res.send(result);
-    }
+        client
+          .db("LinkedList")
+          .collection("Sample data")
+          .findOneAndUpdate(
+            { Post_id: blog.Post_id },
+            { $set: updatedBlog },
+            { ReturnDocument: "after" }
+          );
+      }
+    });
+  } else if (currentBlog.next === null) {
+    allBlog.map((blog) => {
+      if (blog.next === currentBlog.Post_id) {
+        const updatedBlog = {
+          ...blog,
+          next: null,
+        };
+        client
+          .db("LinkedList")
+          .collection("Sample data")
+          .findOneAndUpdate(
+            { Post_id: blog.Post_id },
+            { $set: updatedBlog },
+            { ReturnDocument: "after" }
+          );
+      }
+    });
+  } else {
+    allBlog.map((blog) => {
+      if (blog.next === currentBlog.Post_id) {
+        const updatedBlog = {
+          ...blog,
+          next: currentBlog.next,
+        };
+        client
+          .db("LinkedList")
+          .collection("Sample data")
+          .findOneAndUpdate(
+            { Post_id: blog.Post_id },
+            { $set: updatedBlog },
+            { ReturnDocument: "after" }
+          );
+      }
 
-    async function main() {
-        const uri = URL;
-        client(uri);
-
-        try {
-            await client.connect();
-            const pen = await findAllBlog(client, res);
-        } catch (e) {
-            console.error(e);
-        } finally {
-            await client.close();
-        }
-    }
-    main().catch(console.error);
-
-   
-}
-);
-
+      if (blog.prev === currentBlog.Post_id) {
+        const updatedBlog = {
+          ...blog,
+          prev: currentBlog.prev,
+        };
+        client
+          .db("LinkedList")
+          .collection("Sample data")
+          .findOneAndUpdate(
+            { Post_id: blog.Post_id },
+            { $set: updatedBlog },
+            { ReturnDocument: "after" }
+          );
+      }
+    });
+  }
+  await client
+    .db("LinkedList")
+    .collection("Sample data")
+    .deleteOne({ Post_id: currentBlog.Post_id });
+  res.status(200).send("Successfully Deleted");
+});
 
 app.listen(PORT, () => {
-    console.log("server started at http://localhost:8080/");
-})
-
+  console.log("server started at http://localhost:8080/");
+});
