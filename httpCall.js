0c77b28ff014fcb49971e641714bf20cb4dce819
RakulAgn <rakul0agn@gmail.com>
2022-03-31 08:55:30 +0530

    Not Fixed

--- httpCall.js
+++ httpCall.js
@@ -1,120 +1,117 @@
 function displayAllData() {
-    var httpRequestForShowAll = new XMLHttpRequest(); // HTTPS CALLS
+  var httpRequestForShowAll = new XMLHttpRequest(); // HTTPS CALLS
 
-    function display(a, b, PostId) {
+  function display(a, b, PostId) {
+    let mainDiv = document.querySelector(".main-container");
 
-        let mainDiv = document.querySelector('.main-container');
+    let card = document.createElement("div");
+    card.className = "card card-3";
+    mainDiv.appendChild(card);
 
-        let card = document.createElement('div');
-        card.className = "card card-3";
-        mainDiv.appendChild(card);
+    title = document.createElement("h4");
+    title.className = "card__title";
+    title.id = "cardTitle";
+    title.innerHTML = `Blog Title: ${a}`;
+    card.appendChild(title);
 
-        title = document.createElement('h4');
-        title.className = "card__title";
-        title.id = "cardTitle";
-        title.innerHTML = `Blog Title: ${a}`;
-        card.appendChild(title);
+    description = document.createElement("p");
+    description.className = "content";
+    description.innerHTML = `Blog Content: ${b}`;
+    description.id = "cardDescription";
+    card.appendChild(description);
 
-        description = document.createElement('p');
-        description.className = "content";
-        description.innerHTML = `Blog Content: ${b}`;
-        description.id = "cardDescription";
-        card.appendChild(description);
+    descriptionTwo = document.createElement("p");
+    descriptionTwo.className = "card__apply";
+    descriptionTwo.id = "cardDescriptionTwo";
+    card.appendChild(descriptionTwo);
 
-        descriptionTwo = document.createElement('p');
-        descriptionTwo.className = "card__apply";
-        descriptionTwo.id = "cardDescriptionTwo";
-        card.appendChild(descriptionTwo);
+    let button = document.createElement("button");
+    button.innerHTML = "Delete";
+    button.addEventListener("click", function () {
+      httpRequestForShowAll.open(
+        "DELETE",
+        `http://localhost:8080/deleteBlog/${PostId}`,
+        true
+      );
+      httpRequestForShowAll.send();
+      console.log("Data Deleted");
+    });
+    descriptionTwo.appendChild(button);
+  }
 
-        let button = document.createElement('button');
-        button.innerHTML = "Delete"
-        button.addEventListener("click", function () {
-            httpRequestForShowAll.open("DELETE", `http://localhost:8080/deleteBlog/${PostId}`, true);
-            httpRequestForShowAll.send();
-            console.log("Data Deleted");
+  httpRequestForShowAll.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let myData = JSON.parse(this.response);
 
-        });
-        descriptionTwo.appendChild(button);
+      console.log(myData);
+      for (let i = 0; i < myData.length; i++) {
+        let t = myData[i].BlogTitle;
+        let p = myData[i].BlogContent;
+        let id = myData[i].Post_id;
 
+        display(t, p, id); // Skeleton Template
+      }
     }
-
-
-    httpRequestForShowAll.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            let myData = JSON.parse(this.response)
-
-            console.log(myData);
-            for (let i = 0; i < myData.length; i++) {
-                let t = myData[i].BlogTitle;
-                let p = myData[i].BlogContent;
-                let id = myData[i].Post_id
-
-                display(t, p, id); // Skeleton Template
-            }
-        }
-    };
-    httpRequestForShowAll.open("GET", "http://localhost:8080/allblog", true);
-    httpRequestForShowAll.send();
+  };
+  httpRequestForShowAll.open("GET", "http://localhost:8080/allblog", true);
+  httpRequestForShowAll.send();
 }
 
 function displayOne() {
+  var httpRequestForOneData = new XMLHttpRequest();
+  httpRequestForOneData.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let myData = JSON.parse(this.response);
 
-    var httpRequestForOneData = new XMLHttpRequest();
+      let mainDiv = document.querySelector(".main-container");
 
-    function displayoneData(a, b, next, prev) {
+      let card = document.createElement("div");
+      card.className = "card card-3";
+      mainDiv.appendChild(card);
 
-        let mainDiv = document.querySelector('.main-container');
+      title = document.createElement("h4");
+      title.className = "card__title";
+      title.id = "cardTitle";
+        title.innerHTML = `Blog Title: ${myData.BlogTitle}`;
+      card.appendChild(title);
 
-        let card = document.createElement('div');
-        card.className = "card card-3";
-        mainDiv.appendChild(card);
+      description = document.createElement("p");
+      description.className = "content";
+      description.innerHTML = `Blog Content: ${myData.BlogContent}`;
+      description.id = "cardDescription";
+      card.appendChild(description);
 
-        title = document.createElement('h4');
-        title.className = "card__title";
-        title.id = "cardTitle";
-        title.innerHTML = `Blog Title: ${a}`;
-        card.appendChild(title);
+      descriptionTwo = document.createElement("p");
+      descriptionTwo.className = "card__apply";
+      descriptionTwo.id = "cardDescriptionTwo";
+      card.appendChild(descriptionTwo);
 
-        description = document.createElement('p');
-        description.className = "content";
-        description.innerHTML = `Blog Content: ${b}`;
-        description.id = "cardDescription";
-        card.appendChild(description);
+      let button = document.createElement("button");
+      button.innerHTML = "Prev";
+      button.addEventListener("click", function () {
+          httpRequestForOneData.open(
+          "GET",
+          `http://localhost:8080/getSingleBlog/${myData[0].prev}`,
+          true
+        );
+          httpRequestForOneData.send();
+      });
+      descriptionTwo.appendChild(button);
 
-        descriptionTwo = document.createElement('p');
-        descriptionTwo.className = "card__apply";
-        descriptionTwo.id = "cardDescriptionTwo";
-        card.appendChild(descriptionTwo);
-
-        let button = document.createElement('button');
-        button.innerHTML = "Next"
-        descriptionTwo.appendChild(button);
-
-        let button2 = document.createElement('button');
-        button.innerHTML = "Prev"
-        descriptionTwo.appendChild(button2);
+      let button2 = document.createElement("button");
+      button2.innerHTML = "Next";
+      button2.addEventListener("click", function () {
+          httpRequestForOneData.open(
+          "GET",
+          `http://localhost:8080/getSingleBlog/${myData[0].next}`,
+          true
+        );
+          httpRequestForOneData.send();
+      });
+      descriptionTwo.appendChild(button2);
     }
+  };
 
-    httpRequestForOneData.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            let myData = JSON.parse(this.response)
-
-        
-            for (let i = 0; i < myData.length; i++) {
-                let BlogTitle = myData[0].BlogTitle;
-                let BlogContent = myData[0].BlogContent;
-                let prevousId = myData[0].prev;
-                let nextId = myData[0].next;
-
-                displayoneData(BlogTitle, BlogContent, prevousId, nextId)
-            }
-            httpRequestForOneData.open("GET", `http://localhost:8080/getSingleBlog/${postId}`, true);
-            httpRequestForOneData.send();
-
-        }
-        httpRequestForOneData.open("GET", "http://localhost:8080/allblog", true);
-        httpRequestForOneData.send();
-    };
-
+  httpRequestForOneData.open("GET", `http://localhost:8080/allblog`, true);
+  httpRequestForOneData.send();
 }
-
